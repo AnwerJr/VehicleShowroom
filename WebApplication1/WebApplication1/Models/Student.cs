b51389dc831a43276ab2435ae29eaf12fdb3e8a1
Amr <amr.anwer1111@gmail.com>
2025-11-17 08:52:39 +0200

    Delete WebApplication1 directory

--- WebApplication1/WebApplication1/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public String Name { get; set; }
-        public String ImageURL { get; set; }
-    }
-}
